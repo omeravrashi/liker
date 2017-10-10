d836f7f96daf7de4c8d33290a120bd9e09233311
[omeravrashi] <[avromer@gmail.com]>
2017-10-10 17:40:44 +0300

    Start

---
+++ another2.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+import paho.mqtt.client as mqttClient
+app = Flask(__name__)
+
+broker_address= "m12.cloudmqtt.com"
+port = 15881
+user = "baaagjli"
+password = "qAriS-uGhGQ4"
+client = mqttClient.Client("Python")
+client.username_pw_set(user, password=password)
+client.connect(broker_address, port=port)
+
+json = []
+
+@app.route('/', methods=['GET'])
+def returnAll():
+    return jsonify({'json': json})
+
+
+@app.route('/', methods=['POST'])
+def addOne():
+    new_json = request.get_json()
+    json.append(new_json)
+    client.publish("python/test",str(new_json))
+    return jsonify({'json': json})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
